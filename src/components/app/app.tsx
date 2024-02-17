5ab9a27db9b5fa02ac404f65087db47fbfd60dde
Артур <artur.ayup@gmail.com>
2024-02-17 18:38:26 +0400

    Добавит React-компоненты

---
+++ src/components/app/app.tsx
@@ -0,0 +1,15 @@
+import MainScreen from '../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  offersCount: number;
+}
+
+function App({offersCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      offersCount = {offersCount}
+    />
+  );
+}
+
+export default App;
