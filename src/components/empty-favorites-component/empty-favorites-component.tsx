698fd44e0f220ae22c01384e53b1893848ad8158
Артур <artur.ayup@gmail.com>
2024-02-18 15:33:26 +0400

    Переопределяет директории для компонентов

--- src/components/empty-favorites-component/empty-favorites-component.tsx
+++ src/components/empty-favorites-component/empty-favorites-component.tsx
@@ -1,4 +1,4 @@
-function FavoriteEmptyComponent(): JSX.Element {
+function EmptyFavoritesComponent(): JSX.Element {
   return (
     <main className="page__main page__main--favorites page__main--favorites-empty">
       <div className="page__favorites-container container">
@@ -14,4 +14,4 @@ function FavoriteEmptyComponent(): JSX.Element {
   );
 }
 
-export default FavoriteEmptyComponent;
+export default EmptyFavoritesComponent;
