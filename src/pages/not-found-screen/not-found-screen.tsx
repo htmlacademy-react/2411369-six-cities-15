30360012b1d0ce806059dc4aecc216413a91087e
Артур <artur.ayup@gmail.com>
2024-02-27 22:32:07 +0400

    Обновит компонент страницы "404"

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,21 +1,15 @@
 import { Link } from 'react-router-dom';
-import LogoComponent from '../../components/logo-component/logo-component';
 import style from './not-found-screen.module.css';
 import { useDocumentTitle } from '../../hooks/document-title';
+import HeaderComponent from '../../components/header-component/header-component';
 
 
 function NotFoundScreen(): JSX.Element {
   useDocumentTitle('Not found');
 
   return (
-    <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <LogoComponent />
-          </div>
-        </div>
-      </header>
+    <div className="page">
+      <HeaderComponent />
 
       <main className="page__main page__main--index">
         <h1 className={style.container}>
