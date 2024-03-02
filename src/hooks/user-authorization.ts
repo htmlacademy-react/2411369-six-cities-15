997001af8b259ad873142cd70d4b452306a90b07
Артур <artur.ayup@gmail.com>
2024-03-02 20:16:29 +0400

    Добавляет новых хук авторизации

---
+++ src/hooks/user-authorization.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+type AuthState = {
+  isAuthenticated: boolean;
+  userId: string | null;
+}
+
+const useAuth = (): [AuthState, (usedId: string | null) => void, () => void] => {
+  const [authState, setAuthState] = useState<AuthState>({
+    isAuthenticated: false,
+    userId: null
+  });
+
+  const login = (userId: string | null) => {
+    setAuthState((prevState) => ({
+      ...prevState,
+      isAuthenticated: true,
+      userId
+    }));
+  };
+
+  const logout = () => {
+    setAuthState({
+      isAuthenticated: false,
+      userId: null
+    });
+  };
+
+  return [authState, login, logout];
+};
+
+export default useAuth;
