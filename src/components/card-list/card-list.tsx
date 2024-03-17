90794f0a062ebf5f0008b02295213cd833fba2e6
Артур <artur.ayup@gmail.com>
2024-03-17 21:07:49 +0400

    Добавляет логику отрисовки офферов

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -7,7 +7,8 @@ import Map from '../map/map';
 import { CityName } from '../../const';
 import { useActionCreators } from '../../hooks/store';
 import { offersActions } from '../../store/slice/offers';
-import { MouseEvent } from 'react';
+import { MouseEvent, useState } from 'react';
+import { SortOption } from '../sort';
 
 type CardListProps = {
   currentCity: CityName;
@@ -17,6 +18,7 @@ type CardListProps = {
 
 function CardList({currentCity, currentOffers, isEmpty}: CardListProps) {
   const { setActiveId } = useActionCreators(offersActions);
+  const [ activeSort, setActiveSort ] = useState(SortOption.Popular);
 
   const handleMouseEnter = (evt: MouseEvent<HTMLElement>) => {
     const target = evt.currentTarget as HTMLElement;
@@ -28,6 +30,18 @@ function CardList({currentCity, currentOffers, isEmpty}: CardListProps) {
     setActiveId(undefined);
   };
 
+  let sortedOffers = currentOffers;
+
+  if (activeSort === SortOption.PriceLowToHigh) {
+    sortedOffers = currentOffers.toSorted((a: Offer, b: Offer) => a.price - b.price);
+  }
+  if (activeSort === SortOption.PriceHighToLow) {
+    sortedOffers = currentOffers.toSorted((a: Offer, b: Offer) => b.price - a.price);
+  }
+  if (activeSort === SortOption.TopRatedFirst) {
+    sortedOffers = currentOffers.toSorted((a: Offer, b: Offer) => b.rating - a.rating);
+  }
+
   return (
     <div className= {classNames('container', 'cities__places-container', {'cities__places-container--empty': isEmpty})}>
       {isEmpty ? <CardListEmpty city={currentCity} /> : (
@@ -35,10 +49,10 @@ function CardList({currentCity, currentOffers, isEmpty}: CardListProps) {
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
             <b className="places__found">{currentOffers.length} place{currentOffers.length > 1 && 's'} to stay in {currentCity} </b>
-            <Sort />
+            <Sort current={activeSort} setter={setActiveSort} />
             <div className="cities__places-list places__list tabs__content">
-              {currentOffers.map((offer) => (
-                <Card key={offer.id} {...offer} environment="cities" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+              {sortedOffers.map((offer) => (
+                <Card key={offer.id} environment="cities" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} {...offer} />
               ))}
             </div>
           </section>
