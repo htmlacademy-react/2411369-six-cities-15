acba082c036061927947c141c6db9e0ce8813a06
Артур <artur.ayup@gmail.com>
2024-03-23 15:59:15 +0400

    Подключает redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { offersSlice } from './slice/offers';
+import { createAPI } from '../services/api';
 
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: {
     [offersSlice.name]: offersSlice.reducer,
   },
+  middleware: (getDefaulMiddleware) =>
+    getDefaulMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
