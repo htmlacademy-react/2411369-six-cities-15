035b678a371b1cc2aa87b0ed94a81d908af81bc6
Артур <artur.ayup@gmail.com>
2024-03-23 15:49:23 +0400

    fix: Исправляет привязку класса в зависимости от старницы

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,26 +1,31 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Navigation from '../navigation/navigation';
-import { AppRoute } from '../../const';
+import { AppRoute, CITIES } from '../../const';
 import Logo from '../logo/logo';
+import classNames from 'classnames';
+
+const classPage: Record<string, string> = {
+  [AppRoute.Login]: 'page--gray page--login',
+  [AppRoute.Favorites]: '',
+};
+
+const citiesRotes = new Set(CITIES.map((city) => `/${city.id}`));
 
 function Header(): JSX.Element {
-  const { pathname } = useLocation() as { pathname: AppRoute };
-
-  const classPage = {
-    [AppRoute.Main]: 'page--gray page--main',
-    [AppRoute.Login]: 'page--gray page--login',
-    [AppRoute.Favorites]: '',
-    [AppRoute.Offer]: '',
-    [AppRoute.NotFound]: ''
-  };
+  const { pathname } = useLocation();
+  const pathName = pathname as AppRoute;
+  const isOnMain = pathName === AppRoute.Main || citiesRotes.has(pathname);
 
   return (
-    <div className={`page ${classPage[pathname]}`}>
+    <div className={classNames('page', classPage[pathname], {
+      'page--gray page--main': isOnMain,
+    })}
+    >
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <Logo pathname={pathname} />
+              <Logo isOnMain={isOnMain} />
             </div>
             <Navigation pathname={pathname} />
           </div>
