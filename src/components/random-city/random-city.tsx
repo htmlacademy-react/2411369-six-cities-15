9b6ba07c734b52cab47fb9aaa8b10cd06cf38cd2
Артур <artur.ayup@gmail.com>
2024-03-30 19:41:17 +0400

    Добавляет функцию для получения случайного города

--- src/components/random-city/random-city.tsx
+++ src/components/random-city/random-city.tsx
@@ -1,9 +1,9 @@
-import { faker } from '@faker-js/faker';
 import { CITIES } from '../../const';
 import { Link } from 'react-router-dom';
+import { getRandomArrayElement } from '../../utils';
 
 export function RandomCity(): JSX.Element {
-  const {id, name} = faker.helpers.arrayElement(CITIES);
+  const {id, name} = getRandomArrayElement(CITIES);
   return (
     <section className="locations locations--login locations--current">
       <div className="locations__item">
--- src/utils.ts
+++ src/utils.ts
@@ -61,3 +61,13 @@ export const ucFirst = (string: string) => {
 
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+const getRandomInteger = (min: number, max: number) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+export const getRandomArrayElement = <Element>(array: Element[] | readonly Element[]) =>
+  array[getRandomInteger(0, array.length - 1)];
